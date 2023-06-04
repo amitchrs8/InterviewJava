912cdba7e702b94c4db0b11c21f04129ba5bcbd1
amitchrs8 <amitchrs8@gmail.com>
2023-06-04 21:04:11 +0530

    updated file

--- InterviewJava/src/main/java/basics/StarterMisc.java
+++ InterviewJava/src/main/java/basics/StarterMisc.java
@@ -3,7 +3,7 @@ package basics;
 public class StarterMisc {
 
 	public static void main(String[] args) {
-		System.out.println("Testing the PRoject ");
+		System.out.println("Testing the PRoject  1 ");
 	}
 	
 	
