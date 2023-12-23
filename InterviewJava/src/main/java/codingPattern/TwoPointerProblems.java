91dd4efe52d4e9324aebd8d58b83438eb10a40b1
amitchrs8 <amitchrs8@gmail.com>
2023-12-23 20:44:07 +0530

    Adding Code

---
+++ InterviewJava/src/main/java/codingPattern/TwoPointerProblems.java
@@ -0,0 +1,23 @@
+package codingPattern;
+
+public class TwoPointerProblems {
+
+    public static boolean isPalindrome(String s) {
+        if(s==null || s.length()<=1){
+            return Boolean.TRUE;
+        }
+        for(int i=0,j=s.length()-1;i<j;i++,j--){
+            if(s.charAt(i) != s.charAt(j)){
+                return Boolean.FALSE;
+            }
+        }
+        return Boolean.TRUE;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Pallindrome Check : "+isPalindrome(""));
+    }
+
+
+
+}
